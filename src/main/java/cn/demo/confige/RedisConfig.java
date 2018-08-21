74725d25cf11e25dc3bd7b4fe135367d861e4d11
memeda3n <dongtaitou123@163.com>
2018-08-21 13:27:39 +0800

    整合redis

---
+++ src/main/java/cn/demo/confige/RedisConfig.java
@@ -0,0 +1,96 @@
+package cn.demo.confige;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+	@Value("${spring.redis.host}")
+	private String host;
+	
+	@Value("${spring.redis.port}")
+	private int port;
+	
+	@Value("${spring.redis.database}")
+	private int dataBase;
+	
+	@Value("${spring.redis.timeout}")
+	private int timeout;
+	
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		 RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration ();
+	        redisStandaloneConfiguration.setHostName(host);
+	        redisStandaloneConfiguration.setPort(port);
+	        redisStandaloneConfiguration.setDatabase(dataBase);
+	        redisStandaloneConfiguration.setPassword(RedisPassword.of(""));
+	        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+	        jedisClientConfiguration.connectTimeout(Duration.ofMillis(timeout));//  connection timeout
+	        JedisConnectionFactory factory = new JedisConnectionFactory(redisStandaloneConfiguration,
+	                jedisClientConfiguration.build());
+	        return factory;
+	}
+/*	@Bean
+	public RedisTemplate<String, Object> setRedisTemplate() {
+		RedisTemplate<String, Object> redisTemplate=new RedisTemplate<>();
+		StringRedisSerializer  stringSerializer = new StringRedisSerializer();
+		JdkSerializationRedisSerializer jdkSerializer = new JdkSerializationRedisSerializer();
+		redisTemplate.setConnectionFactory(jedisConnectionFactory());
+		redisTemplate.setKeySerializer(stringSerializer);
+		redisTemplate.setValueSerializer(jdkSerializer);
+		redisTemplate.setHashKeySerializer(stringSerializer);
+		redisTemplate.setHashValueSerializer(jdkSerializer);
+		return redisTemplate;
+	}*/
+
+	  /**
+     * RedisTemplate配置
+     * @param factory
+     * @return
+     * 
+     *参照 https://www.cnblogs.com/liuchuanfeng/p/7009027.html
+     * 
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        //定义key序列化方式
+        //RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long类型会出现异常信息;需要我们上面的自定义key生成策略，一般没必要
+        //定义value的序列化方式
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+       // template.setKeySerializer(redisSerializer);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+    
+    
+}
--- src/main/java/cn/demo/controller/UserController.java
+++ src/main/java/cn/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,24 @@ public class UserController {
 
 	@Resource
 	private UserService userService;
+	@Resource
+	private RedisTemplate<String, Object> redisTemplate;
+	@Resource(name = "redisTemplate")
+	private ValueOperations<Object, Object> valOpsObj;
 	@GetMapping("user")
 	public List<User> user(){
+		String key="1_user4";
+		redisTemplate.opsForValue().set(key, "张三4");
 		System.out.println(" this is user controller");
+		
 		List<User> users=userService.getUsers();
 		System.out.println(users.size());
+		System.out.println(valOpsObj.get(key));
 		return users;
 	}
+	
+	
+	
+	
+	
 }
